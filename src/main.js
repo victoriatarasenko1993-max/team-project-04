c4aaaeb14c34e6e29cae81e623afe3fece1e4d1e
victoriatarasenko1993-max <victoriatarasenko1993@gmail.com>
2026-05-22 19:37:38 +0300

    js+font

---
+++ src/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import './js/header.js';
+import './js/hero.js';
+import './js/dessert-list.js';
+import './js/feedback.js';
+import './js/faq.js';
+import './js/about-us.js';
+import './js/footer.js';
+import './js/mobmenu.js';
+import './js/details-modal.js';
+import './js/order-modal.js';
